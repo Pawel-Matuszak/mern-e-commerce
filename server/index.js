9d0ffe94541a1e99782ed2e1f72ce9299317a720
pawelmatuszak <pawelxmatuszak@gmail.com>
2022-01-23 22:12:00 +0100

    setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+import express from "express"
+import mongoose from "mongoose"
 const app = express()
-const mongoose = require("mongoose")
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`App listening on port ${port}!`))
