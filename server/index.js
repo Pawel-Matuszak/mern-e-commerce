7276a3b169a2aecfbc03455b56cec6f72d1ef86d
pawelmatuszak <pawelxmatuszak@gmail.com>
2022-01-23 22:47:53 +0100

    db setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 import express from "express"
 import mongoose from "mongoose"
-const app = express()
 
+const app = express()
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//Routes
+
+//Mongodb connect
+const DB_URI = process.env.DB_URI;
+
+const main = async () => {
+  await mongoose.connect(DB_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+}
+
+main().catch(err => console.error(err));
+
 app.listen(port, () => console.log(`App listening on port ${port}!`))
