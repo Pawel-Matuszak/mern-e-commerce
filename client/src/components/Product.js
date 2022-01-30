4a05fa7b9ae3dc82c09a51cc0feff06391d0fd6d
pawelmatuszak <pawelxmatuszak@gmail.com>
2022-01-30 18:27:43 +0100

    style

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Product = ({itemDetails}) => {
   return (
     <div className='product'>
-      <img src={itemDetails.url} alt={itemDetails.url} />
+      <img src={`${process.env.PUBLIC_URL}/res/${itemDetails.url}.jpg`}alt={itemDetails.url} />
       <div className="product-name">{itemDetails.name}</div>
       <div className="product-price">{itemDetails.price/100} USD</div>
     </div>
