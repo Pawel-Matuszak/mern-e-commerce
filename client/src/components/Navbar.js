001405f740999f31ab8cf9a2e7fed1bea3b1aeac
pawelmatuszak <pawelxmatuszak@gmail.com>
2022-05-12 14:53:35 +0200

    dropdown and checkout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {ReactComponent as SVGdropdown} from "../images/svgexport-2.svg";
+import CartImg from "../images/checkout_small.webp";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <ul className='navbar-list'>
-        <li>home</li>
-        <li>catalog</li>
-        <li>contact</li>
-      </ul>
-      <div className="cart">cart</div>
-    </div>
+    <nav className='navbar'>
+      <NavDropdown icon={<SVGdropdown/>}>
+        <NavItem>1</NavItem>
+        <NavItem>2</NavItem>
+      </NavDropdown>
+      <div className="title">berd</div>
+      <div className="cart"><img src={CartImg} alt={CartImg}/></div>
+    </nav>
   )
 };
+
+const NavDropdown = (props) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="menuwrap">
+      <span className="navIcon" onClick={()=>{setOpen(!open)}}>{props.icon}</span>
+      <ul className='navbar-dropdown'>
+        {open && props.children}
+      </ul>
+    </div>
+  )
+}
+
+const NavItem = (props) => {
+  return (
+    <li className="dropdown-item">
+      <a href="">{props.children}
+        <img src={props.src} alt={props.src} />      
+      </a>
+    </li>
+  )
+}
 
 export default Navbar;
