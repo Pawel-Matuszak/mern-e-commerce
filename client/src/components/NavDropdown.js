bac09baf017e521d70ae8ab4f055ab3d876cf339
pawelmatuszak <pawelxmatuszak@gmail.com>
2022-05-13 16:58:33 +0200

    menu animations

--- client/src/components/NavDropdown.js
+++ client/src/components/NavDropdown.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {ReactComponent as SVGclose} from "../images/svgexport-1.svg";
+import { motion } from "framer-motion";
 
 const NavDropdown = (props) => {
   const [open, setOpen] = useState(false);
@@ -8,11 +9,13 @@ const NavDropdown = (props) => {
     <div className="navbar-wrap">
       <span className="navIcon" onClick={()=>{setOpen(!open)}}>{props.icon}</span>
       {open && 
-      <ul className='navbar-dropdown'>
-        <li className="dropdown-item dropdown-exit" onClick={()=>setOpen(false)}><SVGclose/></li>
-        {props.children}
+      <motion.ul className='navbar-dropdown' animate={{x:300}} transition={{duration: .5}}>
+        <motion.span animate={{y:[50, 0], opacity:[0, 1]}} transition={{delay: .3, duration: .5}}>
+          <li className="dropdown-item dropdown-exit" onClick={()=>setOpen(false)}><SVGclose/></li>
+          {props.children}
+        </motion.span>
         <div className="cover" onClick={()=>setOpen(false)}></div>
-      </ul>}
+      </motion.ul>}
     </div>
   )
 }
