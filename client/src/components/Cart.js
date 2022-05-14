05b5b56dc833116e4bc8b58379a5f0477af26d23
pawelmatuszak <pawelxmatuszak@gmail.com>
2022-05-14 17:19:57 +0200

    cart grid

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -4,8 +4,20 @@ const Cart = () => {
   return (
     <div className='cart-container'>
       <h1>cart</h1>
-      <p>your cart is currently empty.</p>
-      <p>continue browsing <a href="">here</a></p>
+      {/* <p>your cart is currently empty.</p>
+      <p>continue browsing <a href="">here</a></p> */}
+
+      <div className="items-grid">
+        <div className="header">
+          <div className="label">QUANTITY</div>
+          <div className="label">TOTAL</div>
+        </div>
+        <div className="items">dasdad</div>
+        <div className="summary">
+          <div className="label">subtotal</div>
+          <div className="label price">$22.22</div>
+        </div>
+      </div>
     </div>
   )
 }
