53c49f9496cc66b81f260c2d55bae3c0420cc415
pawelmatuszak <pawelxmatuszak@gmail.com>
2022-05-30 17:12:15 +0200

    a

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use("/products", productsRoutes);
 if(process.env.NODE_ENV === "production"){
   app.use(express.static('client/build'));
 
-  app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+  app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
 
